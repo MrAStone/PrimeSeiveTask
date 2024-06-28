23b41b0a60191e78511ca61c4685a0634aa59105
Al Stone <astone@heckgrammar.co.uk>
2024-06-28 08:48:53 +0100

    Program outputs number of primes

--- PrimeSeiveTask/Program.cs
+++ PrimeSeiveTask/Program.cs
@@ -8,7 +8,7 @@
             Console.Write("Enter number to find all primes up to: ");
             maxNum = Convert.ToInt32(Console.ReadLine());
             List<int> PrimeNumbers = PrimeSeive(maxNum);
-
+            Console.WriteLine(PrimeNumbers.Count);
         }
 
         static List<int> PrimeSeive(int MaxNumber)
@@ -17,12 +17,18 @@
             bool[] numbers = new bool[MaxNumber+1];
             //Set all numbers in the array from 2 to MaxNumber to true
             // for i = 2, 3, 4, ..., not exceeding √n do
-            // if A[i] is true
+            // if numbers[i] is true
             //    for j = i2, i2 + i, i2 + 2i, i2 + 3i, ..., not exceeding n do
-            //            set A[j] := false
+            //            set numbers[j] := false
 
 
-
+            for(int i = 0;i<numbers.Length;i++)
+            {
+                if (numbers[i])
+                {
+                    primes.Add(i);
+                }
+            }
             return primes;
         }
     }
